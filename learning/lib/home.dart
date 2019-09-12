96bc784f8acf914aa44c9b65b0782a73d75cde06
cheapthrillandwine <cheapthrillandwine@gmail.com>
2019-09-13 01:41:42 +0900

    initial commit

---
+++ learning/lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:learning/tab.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabUI();
+  }
+}
+
