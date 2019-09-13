ac559b3289427f7908d772eafd85bff29acc5627
cheapthrillandwine <cheapthrillandwine@gmail.com>
2019-09-13 12:10:03 +0900

    Edited README

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This is my flutter study code library.
 # Index
 
 - Location
+
+![Permission](https://github.com/cheapthrillandwine/i-am-learning-flutter/blob/images/images/location_permission.PNG)
+
+![Loading](https://github.com/cheapthrillandwine/i-am-learning-flutter/blob/images/images/location_loading.PNG)
+
+![Current State](https://github.com/cheapthrillandwine/i-am-learning-flutter/blob/images/images/location_currentlocation.PNG)
--- learning/ios/Flutter/flutter_export_environment.sh
+++ learning/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ryoichikataoka/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/ryoichikataoka/i-am-learning-flutter/learning"
-export "FLUTTER_TARGET=/Users/ryoichikataoka/i-am-learning-flutter/learning/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/ryoichikataoka/flutter/bin/cache/artifacts/engine/ios"
